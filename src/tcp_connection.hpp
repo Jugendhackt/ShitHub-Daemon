7617692aea36016fc8c080af6b84409bb83a8ae6
anghenfil <keanu.doelle@gmx.de>
2018-01-11 10:25:23 +0100

    Removed unused methods

--- src/tcp_connection.hpp
+++ src/tcp_connection.hpp
@@ -11,35 +11,33 @@
 class tcp_connection : public boost::enable_shared_from_this<tcp_connection>
 {
 public:
-	boost::asio::local::stream_protocol::socket& socket(); //Returns socket
-	void start(); //Starts reading, send response to sender
-	void send_msg(std::string); //Sends msg to sender
-  	static boost::shared_ptr<tcp_connection> create(boost::asio::io_service&); //Create shred_ptr object
-  	
-private: 
-	boost::asio::streambuf read_buffer; //Buffer for async reading
-	//typedef boost::shared_ptr<tcp_connection> pointer; //for tcp_connection::create
-	
-	boost::asio::local::stream_protocol::socket socket_; //actual socket
-  	std::string message; //Welcome message
+    boost::asio::local::stream_protocol::socket& socket(); //Returns socket
+    void start(); //Starts reading, send response to sender
+    void send_msg(std::string); //Sends msg to sender
+    static boost::shared_ptr<tcp_connection> create(boost::asio::io_service&); //Create shred_ptr object
 
-  	tcp_connection(boost::asio::io_service& io_service) : socket_(io_service){
+private:
+    boost::asio::streambuf read_buffer; //Buffer for async reading
 
-  	}
+    boost::asio::local::stream_protocol::socket socket_; //actual socket
+    std::string message; //Welcome message
 
-  	void read(); //Reads senders message
-  	std::string make_string(boost::asio::streambuf&); //Converts streambuf to string
+    tcp_connection(boost::asio::io_service& io_service) : socket_(io_service){
 
- 	std::vector<size_t> get_positions(std::string, std::string); //searchstring and message
+    }
 
-	void split_msgs(std::string); //Splits the string in several "messages". Later with return -> no void
+    void read(); //Reads senders message
+    std::string make_string(boost::asio::streambuf&); //Converts streambuf to string
 
-	void handle_read(const boost::system::error_code&, size_t); //handles read
-	void handle_write(const boost::system::error_code&, size_t); //handles write
+    std::vector<size_t> get_positions(std::string, std::string); //searchstring and message
 
-	//tcp_connection(boost::asio::io_service& io_service) : socket(io_service);
-	void analyse_input(std::string);
+    void split_msgs(std::string); //Splits the string in several "messages". Later with return -> no void
+
+    void handle_read(const boost::system::error_code&, size_t); //handles read
+    void handle_write(const boost::system::error_code&, size_t); //handles write
+
+    void analyse_input(std::string);
 
 };
-typedef boost::shared_ptr<tcp_connection> pointer; 	
+typedef boost::shared_ptr<tcp_connection> pointer;
 #endif
