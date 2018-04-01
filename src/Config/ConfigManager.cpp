23d0e6bde0ee8e88f8b3cbe69d608bd044771596
anghenfil <keanu.doelle@gmx.de>
2018-04-01 22:24:46 +0200

    Added addConfigStore

--- src/Config/ConfigManager.cpp
+++ src/Config/ConfigManager.cpp
@@ -4,6 +4,8 @@
 
 #include "ConfigManager.hpp"
 
-Config::ConfigManager::ConfigManager() : configstores(){
+Config::ConfigManager::ConfigManager() : configstores(){}
 
+void Config::ConfigManager::addConfigStore(Config::ConfigStore cs) {
+    configstores.insert(std::pair<std::string, Config::ConfigStore>(cs.getPrefix(), cs));
 }
